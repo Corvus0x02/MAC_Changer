30dda9c0713fe00e299d6da86f06de59f020b9e7
Corvus0x02 <Corvus0x02@protonmail.com>
2022-06-20 21:06:06 -0400

    06202022 mac_changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+# original_mac = "02:42:fe:59:94:0e"
+# new_mac = "00:11:22:33:44:55"
 
 import subprocess
+import optparse
 
-interface = "docker0"
-original_mac = "02:42:fe:59:94:0e"
-new_mac = "00:11:22:33:44:55"
+parser = optparse.OptionParser()
+parser.add_option("-i","--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m","--mac", dest="new_mac",help="The new MAC to change the interface to")
 
+parser.parse_args()
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + original_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
