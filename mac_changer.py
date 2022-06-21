5cff2b0439b92401bbd2d4501ec652cd6bc70093
Corvus0x02 <Corvus0x02@protonmail.com>
2022-06-20 20:24:36 -0400

    Updated mac_changer script

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+
 import subprocess
 
-subprocess.call("ifconfig docker0 down", shell=True)
-subprocess.call("ifconfig docker0 hw ether 02:42:fe:59:94:0e", shell=True)
-subprocess.call("ifconfig docker0 up", shell=True)
+interface = "docker0"
+original_mac = "02:42:fe:59:94:0e"
+new_mac = "00:11:22:33:44:55"
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + original_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
